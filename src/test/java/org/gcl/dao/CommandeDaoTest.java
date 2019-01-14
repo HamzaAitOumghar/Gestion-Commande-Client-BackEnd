816f10808538bea4728eceb2a5be6672f021d480
HAMZA AIT OUMGHAR <hamwac5@yahoo.fr>
2019-01-14 20:22:20 +0000

    integration test for CommandeService

--- src/test/java/org/gcl/dao/CommandeDaoTest.java
+++ src/test/java/org/gcl/dao/CommandeDaoTest.java
@@ -1,6 +1,16 @@
 package org.gcl.dao;
 
+import java.util.Date;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.time.Instant;
+
+import org.gcl.entities.Commande;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -8,4 +18,28 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class CommandeDaoTest {
 
+	@Autowired
+	private CommandeRepository commandeRepo;
+	
+	
+	@Test
+	public void addCommande() {
+		
+		Date currentDate = new Date();
+		Commande cmd =new Commande();
+		cmd.setDateCommande(new java.sql.Date(currentDate.getTime()));
+		cmd.setDateLivraison(new java.sql.Date(currentDate.getTime()));
+		cmd.setNumero("CM120");
+		
+		Commande commande = commandeRepo.save(cmd);
+		
+		//Test
+		
+		assertNotNull(commande);
+		assertNotNull(commande.getId());
+		assertEquals("CM120", commande.getNumero());
+		
+		
+	}
+	
 }
